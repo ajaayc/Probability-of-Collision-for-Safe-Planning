83137eca7e2a05e3dad7b8462bbd24dcbf7d02ae
Ajaay Chandrasekaran <ajaay@umich.edu>
2018-04-21 16:13:07 -0400

    Sample from GMM

--- mcsimplugin/GM_Model.h
+++ mcsimplugin/GM_Model.h
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include <armadillo>
+#include <chrono>
+#include <random>
 
 using namespace arma;
 
@@ -14,9 +16,19 @@ class GM_Model{
     //Each covariance is a 3 x 3
     std::vector<arma::Mat<double>> covariances;
 
+    //Random generator
+    std::default_random_engine generator;
+    //Weights of each item in GMM. Same as weighted_dist.probabilities
+    std::vector<double> weights;
+    // in this loaded die, the 6 is three times more likely:
+    std::discrete_distribution<int> weighted_dist;
+
 public:
     //Cstor
-    GM_Model(){}
+    GM_Model(){
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        generator = std::default_random_engine(seed);
+    }
     
     void initModel(int numGaussians,arma::Mat<double>& initialMean, arma::Mat<double> initialCovariance){
         this->numGaussians = numGaussians;
@@ -28,10 +40,24 @@ public:
             means[i] = initialMean;
             covariances[i] = initialCovariance;
         }
+
+        //Initialize with equal weights
+        std::vector<double> weightsn(this->numGaussians, 1.0/this->numGaussians);
+        this->updateWeights(weightsn);
     }
 
     //Sample from GM_Model
+    void sampleVal(){
+        int chosen_gaussian = this->weighted_dist(generator);
+        return;
+    }
 
+
+    //Update distribution with new weights
+    void updateWeights(std::vector<double>& weights){
+        this->weights = weights;
+        this->weighted_dist = std::discrete_distribution<int>(weights.begin(),weights.end());
+    }
         
 };
 
